967e88ed79d7a929f5eec9745b0ba2e61d385a52
michaelkcwong <wong_michael@rogers.com>
2023-03-17 22:18:25 -0400

    Search Wikipedia

--- wikiSearch.js
+++ wikiSearch.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.js-search-form');
 form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   //prevent page from reloading when form is submitted
   event.preventDefault();
   //get the value of the input field
@@ -9,5 +9,21 @@ function handleSubmit(event) {
   //remove whitespace from the input
   const searchQuery = inputValue.trim();
   //print 'searchQuery' to the console
-  console.log(searchQuery);
+  try {
+    const results = await searchWikipedia(searchQuery);
+    console.log(results);
+  } catch (err) {
+    console.log(err);
+    alert('Failed to search wikipedia');
+  }
+}
+
+async function searchWikipedia(searchQuery) {
+  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=&srlimit=20&srsearch=SEARCH_QUERY_GOES_HERE`
+  const response = await fetch(endpoint);
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  const json = await response.json();
+  return json;
 }
